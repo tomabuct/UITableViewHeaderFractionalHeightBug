524c905e3c917415df90233f46048439ace5c0d5
Tom Abraham <tabraham@yelp.com>
2014-05-14 11:25:14 -0700

    repro'd

--- TableViewHeaderFractionalHeightBug/AppDelegate.m
+++ TableViewHeaderFractionalHeightBug/AppDelegate.m
@@ -8,12 +8,17 @@
 
 #import "AppDelegate.h"
 
+#import "ViewController.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.rootViewController = [[ViewController alloc] init];
+  [self.window makeKeyAndVisible];
+
+  return YES;
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
--- TableViewHeaderFractionalHeightBug/ViewController.m
+++ TableViewHeaderFractionalHeightBug/ViewController.m
@@ -12,18 +12,40 @@
 
 @end
 
+NSString *const kCellID = @"Cell";
+
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (instancetype)init {
+  if (self = [super init]) {
+    self.tableView.backgroundColor = [UIColor blueColor];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kCellID];
+
+    UIView *tableHeaderView = [[UIView alloc] init];
+    tableHeaderView.backgroundColor = [UIColor redColor];
+    self.tableView.tableHeaderView = tableHeaderView;
+  }
+  return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellID forIndexPath:indexPath];
+  cell.textLabel.text = indexPath.row == 0 ? @"Make tableHeaderView's height 0.5" : @"Make tableHeaderView's height 1";
+  return cell;
+}
+
+#pragma mark UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  UIView *headerView = tableView.tableHeaderView;
+  headerView.frame = CGRectMake(0, 0, 0, (indexPath.row == 0) ? 0.5 : 1);
+  tableView.tableHeaderView = headerView;
 }
 
 @end
